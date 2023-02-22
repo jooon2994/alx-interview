109c6bbdba9a2b73cb18ee652c857e55a648dd94
jooon2994 <wizyoni7@gmail.com>
2023-02-23 01:10:23 +0300

    new

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,18 +1,28 @@
-#!/user/bin/python3
-"""making change module
+#!/usr/bin/python3
+"""Making change module
 """
 
 
 def makeChange(coins, total):
-    if total <= 0:
+    """Finds the minimum number of coins needed to make change
+    """
+    if (total <= 0):
         return 0
-    dp = [float('inf') for _ in range(total + 1)]
-    dp[0] = 0
-    for i in range(1, total + 1):
-        for coin in coins:
-            if coin <= i:
-                dp[i] = min(dp[i], 1 + dp[i - coin])
-    return dp[total] if dp[total] != float('inf') else -1
+    coin_arr = [float('inf') for i in range(total + 1)]
+    coin_arr[0] = 0
+    for coin in coins:
+        for x in range(len(coin_arr)):
+            if coin <= x:
+                coin_arr[x] = min(coin_arr[x], 1 + coin_arr[x - coin])
+    return coin_arr[total] if coin_arr[total] != float('inf') else -1
+
+
+
+
+
+
+
+
 
 
         
